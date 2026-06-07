4152e9a1d4476d347d764de923a8bb883bb6d52b
aanishparajuli5 <aanishparajuli5@gmail.com>
2026-06-07 11:58:45 +0545

    Implement notification module

--- src/controller/NotificationController.java
+++ src/controller/NotificationController.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-/**
- *
- * @author aanishparajuli
- */
+import dao.NotificationDAO;
+import view.NotificationPanel;
+import javax.swing.JLabel;
+import java.util.List;
+
 public class NotificationController {
-    
+
+    private NotificationPanel panel;
+    private NotificationDAO dao;
+
+    public NotificationController(NotificationPanel panel) {
+        this.panel = panel;
+        this.dao   = new NotificationDAO();
+    }
+
+    public void loadAll() {
+        JLabel[] msgLabels = {
+            panel.getJLabel2(), panel.getJLabel5(), panel.getJLabel8()
+        };
+        JLabel[] timeLabels = {
+            panel.getJLabel3(), panel.getJLabel6(), panel.getJLabel9()
+        };
+
+        List<String[]> rows = dao.getRecentNotifications();
+        int i = 0;
+        for (String[] row : rows) {
+            if (i >= 3) break;
+            msgLabels[i].setText(row[1]);
+            timeLabels[i].setText(row[2]);
+            i++;
+        }
+        // Clear remaining slots
+        for (; i < 3; i++) {
+            msgLabels[i].setText("");
+            timeLabels[i].setText("");
+        }
+    }
 }
