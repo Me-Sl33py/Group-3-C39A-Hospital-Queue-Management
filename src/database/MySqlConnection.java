5179be1eae13b4c2d96ac717489be720725ea1db
Me-Sl33py <manishfie@gmail.com>
2026-06-12 22:44:24 +0545

    update db password to local

--- src/database/MySqlConnection.java
+++ src/database/MySqlConnection.java
@@ -22,7 +22,7 @@ public class MySqlConnection implements DB {
 
     // ==================== Database Configuration ====================
     private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "Aayush@2060";
+    private static final String DB_PASSWORD = "I_!t_Relax!";
     private static final String DB_NAME     = "hospital_queue_management_db";
     private static final String DB_URL      = "jdbc:mysql://localhost:3306/" + DB_NAME;
 
