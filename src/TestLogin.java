839d5083595ebe497f87cf14a677972c9a4bc5e3
Me-Sl33py <manishfie@gmail.com>
2026-06-20 00:21:57 +0545

    done

--- src/TestLogin.java
+++
@@ -1,1 +0,0 @@
-import dao.UserDAO; public class TestLogin { public static void main(String[] args) { UserDAO dao = new UserDAO(); System.out.println(dao.checkLogin("9876543210", "password123")); } }  
